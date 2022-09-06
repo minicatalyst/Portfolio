288e275953fb4429ba4429644da4602c9c084096
minicatalyst <110686233+minicatalyst@users.noreply.github.com>
2022-09-06 11:06:28 -0500

    Update weather.js

--- root/weather.js
+++ root/weather.js
@@ -2,22 +2,23 @@ const form = document.querySelector(".top-banner form");
 const input = document.querySelector(".top-banner input");
 const msg = document.querySelector(".top-banner .msg");
 const list = document.querySelector(".ajax-section .cities");
+/*SUBSCRIBE HERE FOR API KEY: https://home.openweathermap.org/users/sign_up*/
 const apiKey = "c02982ea23b96b12b30f3e1fd1b68d11";
 
 form.addEventListener("submit", e => {
   e.preventDefault();
   let inputVal = input.value;
 
-
+  //check if there's already a city
   const listItems = list.querySelectorAll(".ajax-section .city");
   const listItemsArray = Array.from(listItems);
 
   if (listItemsArray.length > 0) {
     const filteredArray = listItemsArray.filter(el => {
       let content = "";
-
+      //athens,gr
       if (inputVal.includes(",")) {
-
+        //athens,grrrrrr->invalid country code, so we keep only the first part of inputVal
         if (inputVal.split(",")[1].length > 2) {
           inputVal = inputVal.split(",")[0];
           content = el
